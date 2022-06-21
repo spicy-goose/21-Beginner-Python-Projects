f0e792f6efbcf4bf3c8909926052dd3c4b74e8ad
Gabriel Franco <francoxgabriel@gmail.com>
2022-06-21 18:36:08 -0500

    Add computer guessing function and selector function

--- 02-Guess-the-number/main.py
+++ 02-Guess-the-number/main.py
@@ -1,13 +1,9 @@
-from mimetypes import guess_extension
-from random import random
-
-
 import random
 
-def playNumberGuess():
+def playNumberGuess(x):
     guessedCorrectly = False
-    correctNumber = random.randint(0,10)
-    currentGuess = int(input("Select a number between 0 and 10: "))
+    correctNumber = random.randint(0,x)
+    currentGuess = int(input(f"Select a number between 0 and {x}: "))
     while guessedCorrectly == False:
         if currentGuess == correctNumber:
             guessedCorrectly = True
@@ -18,4 +14,31 @@ def playNumberGuess():
         currentGuess = int(input("Too low, try again: "))
     print(f"Congrats! {correctNumber} was the right answer!")
 
-playNumberGuess()
+def pcPlayNumberGuess(x):
+    guessedCorrectly = False
+    correctNumber = int(input(f"Select a number between 0 and {x}: "))
+    lowBound = 0
+    highBound = x
+    
+    while guessedCorrectly == False:
+        currentGuess = random.randint(lowBound, highBound)
+        if currentGuess == correctNumber:
+            guessedCorrectly = True
+            continue
+        if (currentGuess > correctNumber):
+            print(f'Computer picked {currentGuess}, it was too high')
+            highBound = currentGuess-1
+            continue
+        print(f'Computer picked {currentGuess}, it was too low')
+        lowBound = currentGuess+1
+    print(f"Congrats Computer! {correctNumber} was the right answer!")
+
+def decidePlayerGame():
+    decision = input('what guessing game to play? player guess (1) or computer guess (2): ')
+    rangeOfGame = int(input('How big do you want the number for the game?: '))
+    if decision == "1":
+        playNumberGuess(rangeOfGame)
+        return
+    pcPlayNumberGuess(rangeOfGame)
+
+decidePlayerGame()
