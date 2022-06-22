ff87954d5728b16c6a4045116ee697705e43bc1e
Gabriel Franco <francoxgabriel@gmail.com>
2022-06-22 15:36:53 -0500

    Update hangman.py
    - Change lists to sets to remove need of duplicate checks
    - Change structure of conditional statemens for streamlining
    - Change duplicate uses of displayCurrentScore functions
    - Add checks for previous guesses and valid english characters
    - Remove case sensitivity by making all strings lowercase

--- 03-Hangman/hangman.py
+++ 03-Hangman/hangman.py
@@ -1,6 +1,7 @@
 import random
 from sqlite3 import Cursor
 from words import words
+import string
 
 def getValidWord(wordlist):
     indexSelection = random.randint(0, len(wordlist)-1)
@@ -13,67 +14,48 @@ def getValidWord(wordlist):
 def letterIsInWord(letter, word):
     return letter in word
 
-def wordHasDuplicateLetters(letter, word):
-    return word.find(letter) != word.rfind(letter)
-
-def findIndexOfDuplicates(letter, word):
-    indexes = []
-    indexOfLetter = 0
-    while True:
-        indexOfLetter = word.find(letter, indexOfLetter) #finds next index of letter
-        if indexOfLetter == -1:
-            break
-        indexes.append(indexOfLetter)
-        indexOfLetter += 1
-    return indexes
-
-def displayCurrentScore(numberOfLives, currentWordGuess, allLetterArray):
+def displayCurrentScore(numberOfLives, selectedWord, allLettersUsedSet):
+    currentWord = [letter if letter in allLettersUsedSet else "-" for letter in selectedWord]
     print(f"Current number of lives: {numberOfLives}\
-    Letters used: {allLetterArray}\
-    Word: {currentWordGuess}")
+    Letters used: {allLettersUsedSet}\
+    Word: {currentWord}")
     print("\n") #new line for aesthetic purposes
     return
 
 def playHangman():
+
     numberOfLives = 5
     selectedWord = getValidWord(words)
-    correctWord = list(selectedWord)
-    currentWordGuess = list("-" * len(correctWord)) #creates empty initial guess using dashes
+    correctWord = set(selectedWord)
     currentLetterGuess = ""
-    allLetterArray = []
-    
+    allLettersUsedSet = set()
+    alphabet = set(string.ascii_lowercase)
+
     print("Let's play Hangman")
-    displayCurrentScore(numberOfLives, currentWordGuess, allLetterArray)
 
-    while currentWordGuess != correctWord:
+    while len(correctWord) > 0 and numberOfLives > 0:
 
-        if numberOfLives < 1:
-            break
-
-        currentLetterGuess = input("What letter is your guess? ")
+        displayCurrentScore(numberOfLives, selectedWord, allLettersUsedSet)
+        currentLetterGuess = input("What letter is your guess? ").lower()
         print("\n") #new line for aesthetic purposes
+
+        if len(currentLetterGuess) > 1 or currentLetterGuess not in alphabet:
+            print("Invalid guess, Try again!")
+            continue
+        elif currentLetterGuess in allLettersUsedSet:
+            print(f'You already used {currentLetterGuess}, try again!')
+            continue
+
+        allLettersUsedSet.add(currentLetterGuess)
 
         if (letterIsInWord(currentLetterGuess, selectedWord) == False):
             numberOfLives -= 1
-            allLetterArray.append(currentLetterGuess)
             print(f"Sorry, {currentLetterGuess} is not in the word")
-            displayCurrentScore(numberOfLives, currentWordGuess, allLetterArray)
             continue
         
-        elif (wordHasDuplicateLetters(currentLetterGuess, selectedWord) == False):
-            allLetterArray.append(currentLetterGuess)
-            currentWordGuess[selectedWord.find(currentLetterGuess)] = currentLetterGuess
-            print(f'Bingo! {currentLetterGuess} is correct!')
-            displayCurrentScore(numberOfLives, currentWordGuess, allLetterArray)
-            continue
-
-        indexes = findIndexOfDuplicates(currentLetterGuess, selectedWord)
-        for i in indexes:
-            currentWordGuess[i] = currentLetterGuess
+        correctWord.remove(currentLetterGuess)
+        print(f'Bingo! {currentLetterGuess} is correct!')
             
-        allLetterArray.append(currentLetterGuess)    
-        print(f'Bingo! {currentLetterGuess} is correct!')    
-        displayCurrentScore(numberOfLives, currentWordGuess, allLetterArray)
 
     if (numberOfLives < 1):
         print(f"Sorry, you lose. The word we were looking for is '{selectedWord}'!")
